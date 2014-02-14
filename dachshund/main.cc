e2ef836984c563e0d926ce65489c1f7160c21ad2
Casey W. Stark <caseywstark@gmail.com>
2014-02-14 10:06:40 -0800

    Updating main to reflect lib changes. Moved PCG loop inside main for simpler development. Also reorganized the loops and added openmp pragmas.

--- dachshund/main.cc
+++ dachshund/main.cc
@@ -8,6 +8,8 @@
 
 #include <string>
 
+#include <omp.h>
+
 #include "dachshund.h"
 
 void
@@ -17,10 +19,11 @@ print_usage()
     puts("    input     path to config file.");
 }
 
-
 int
 main(int argc, char **argv)
 {
+    Timer *total_timer = new Timer();
+
     // Fancy command line parsing.
     if (argc != 2) {
         fputs("Bad number of arguments. Expected 1.\n", stderr);
@@ -34,23 +37,22 @@ main(int argc, char **argv)
     // Set default params.
     //
 
-    int num_skewers = 100;
-    int num_pixels = 100;
+    // p is the params object declared in the lib.
+    p.num_skewers = 100;
+    p.num_pixels = 100;
+    p.nx = 64;
+    p.ny = 64;
+    p.nz = 64;
+    p.sigma = 1.0;
+    p.l_perp = 1.0;
+    p.l_para = 1.0;
+    p.pcg_tol = 1.0e-3;
+    p.pcg_max_iter = 100;
+
     std::string coords_path = "skewer_coords.bin";
     std::string data_path = "skewer_data.bin";
     std::string weights_path = "skewer_weights.bin";
-    int nx = 64;
-    int ny = 64;
-    int nz = 64;
-    double lx = 100.0;
-    double ly = 100.0;
-    double lz = 100.0;
     std::string output_path = "map.bin";
-    double sigma_F = 0.1;
-    double l_perp = 0.5;
-    double l_para = 1.0;
-    double pcg_tol = 1.0e-3;
-    int pcg_max_iter = 100;
 
     //
     // Parse input/config.
@@ -85,10 +87,10 @@ main(int argc, char **argv)
         sscanf(config_buf, "%s = %s", key, value);
 
         if (strcmp(key, "num_skewers") == 0) {
-            num_skewers = atoi(value);
+            p.num_skewers = atoi(value);
         }
         else if (strcmp(key, "num_pixels") == 0) {
-            num_pixels = atoi(value);
+            p.num_pixels = atoi(value);
         }
         else if (strcmp(key, "coords_path") == 0) {
             coords_path = value;
@@ -100,40 +102,40 @@ main(int argc, char **argv)
             weights_path = value;
         }
         else if (strcmp(key, "nx") == 0) {
-            nx = atoi(value);
+            p.nx = atoi(value);
         }
         else if (strcmp(key, "ny") == 0) {
-            ny = atoi(value);
+            p.ny = atoi(value);
         }
         else if (strcmp(key, "nz") == 0) {
-            nz = atoi(value);
+            p.nz = atoi(value);
         }
         else if (strcmp(key, "lx") == 0) {
-            lx = atof(value);
+            p.lx = atof(value);
         }
         else if (strcmp(key, "ly") == 0) {
-            ly = atof(value);
+            p.ly = atof(value);
         }
         else if (strcmp(key, "lz") == 0) {
-            lz = atof(value);
+            p.lz = atof(value);
         }
         else if (strcmp(key, "output_path") == 0) {
             output_path = value;
         }
         else if (strcmp(key, "sigma_F") == 0) {
-            sigma_F = atof(value);
+            p.sigma = atof(value);
         }
         else if (strcmp(key, "l_perp") == 0) {
-            l_perp = atof(value);
+            p.l_perp = atof(value);
         }
         else if (strcmp(key, "l_para") == 0) {
-            l_para = atof(value);
+            p.l_para = atof(value);
         }
         else if (strcmp(key, "pcg_tol") == 0) {
-            pcg_tol = atof(value);
+            p.pcg_tol = atof(value);
         }
         else if (strcmp(key, "pcg_max_iter") == 0) {
-            pcg_max_iter = atoi(value);
+            p.pcg_max_iter = atoi(value);
         }
         else {
             fprintf(stderr, "Unknown key '%s' in config.\n", key);
@@ -143,17 +145,20 @@ main(int argc, char **argv)
 
     fclose(config_file);
 
-    int64_t num_cells = (int64_t)nx * (int64_t)ny * (int64_t)nz;
-    int64_t num_pixel_elements = (int64_t)num_pixels * (int64_t)num_skewers;
-    double dx = lx / nx;
-    double dy = ly / ny;
-    double dz = lz / nz;
-    double dz_pix = lz / num_pixels;
+    int64_t num_cells = (int64_t)p.nx * (int64_t)p.ny * (int64_t)p.nz;
+    int64_t num_pixel_elements = (int64_t)p.num_pixels * (int64_t)p.num_skewers;
+    p.dx = p.lx / p.nx;
+    p.dy = p.ly / p.ny;
+    p.dz = p.lz / p.nz;
+    p.dz_pix = p.lz / p.num_pixels;
 
-    double *skewer_x = new double[num_skewers];
-    double *skewer_y = new double[num_skewers];
+    double *skewer_x = new double[p.num_skewers];
+    double *skewer_y = new double[p.num_skewers];
     double *data_vec = new double[num_pixel_elements];
     double *weights_vec = new double[num_pixel_elements];
+
+    p.skewer_x = skewer_x;
+    p.skewer_y = skewer_y;
 
     //
     // Read in skewers.
@@ -167,8 +172,8 @@ main(int argc, char **argv)
         exit(1);
     }
 
-    fread(skewer_x, sizeof(double), num_skewers, coords_file);
-    fread(skewer_y, sizeof(double), num_skewers, coords_file);
+    fread(skewer_x, sizeof(double), p.num_skewers, coords_file);
+    fread(skewer_y, sizeof(double), p.num_skewers, coords_file);
     fclose(coords_file);
 
     FILE *data_file = fopen(data_path.c_str(), "r");
@@ -193,73 +198,180 @@ main(int argc, char **argv)
         noise_vec[i] = 1.0 / noise_vec[i];
     }
 
-    // DEBUG
-    // Check input data
-    check_finite(num_skewers, skewer_x);
-    check_finite(num_skewers, skewer_y);
-    check_finite(num_skewers, data_vec);
-    check_finite(num_skewers, weights_vec);
+    p.noise = noise_vec;
+
+    //
+    // Main loop to compute (S + N)^-1 d
+    //
 
     // Create gpt.
     int gt_n = 40;
     double gt_dx = 4.0 / gt_n;
     GT *gt = new GT(gt_n, gt_dx);
+    p.gt = gt;
+
+    printf("Entering main loop with n_i = %i, n_j = %i.\n",
+        (int)num_cells, (int)num_pixel_elements);
+
+    // We store (S + N)^-1 d here...
+    double *b_j = new double[num_pixel_elements];
+
+    // We care about timing starting here.
+    Timer *loop_timer = new Timer();
+
+    // Begin parallel block where we give each thread a block of pixels.
+    #pragma omp parallel
+    {
+        int num_threads = omp_get_num_threads();
+        printf("Running with %i threads.\n", num_threads);
+
+        int k;
+        int n = num_pixel_elements;
+
+        // Allocate thread local arrays.
+        double *a_jk = new double[n];
+        double *d = new double[n];
+        double *r = new double[n];
+        double *s = new double[n];
+        double *q = new double[n];
+
+        // Set initial a_jk guess.
+        for (int k = 0; k < n; ++k) {
+            if (k == 0) {
+                a_jk[k] = 1.0 / sddn_element_func(k, k);
+            }
+            else {
+                a_jk[k] = 0.0;
+            }
+        }
+
+        // The main loop over pixels.
+        # pragma omp for
+        for (int j = 0; j < n; ++j) {
+
+            // DEBUG
+            printf("pixel %i\n", (int)j);
+
+            // Setup the residual.
+            // r = b - A x
+
+            int jj, kk = 0;
+            for (jj = 0; jj < n; ++jj) {
+                // Handle b[i]
+                if (jj == j) {
+                    r[jj] = 1.0;
+                }
+                else {
+                    r[jj] = 0.0;
+                }
+
+                //r[i] -= A(i, j) * x[j];
+                for (kk = 0; kk < n; ++kk) {
+                    r[jj] -= sddn_element_func(jj, kk) * a_jk[kk];
+                }
+            }
+
+            // The preconditioning.
+            // For now, we will just try a Jacobian M (A diag).
+            // s = M^-1 r
+            for (jj = 0; jj < n; ++jj) {
+                d[jj] = r[jj] / sddn_element_func(jj, jj);
+            }
+
+            // delta = r^T d
+            double delta_new = 0.0;
+            for (jj = 0; jj < n; ++jj) {
+                delta_new += r[jj] * d[jj];
+            }
+
+            double delta_0 = delta_new;
+
+            // Handle singular case.
+            //if (delta_0 == 0.0) { return; }
+
+            // the iteration count.
+            int iter = 0;
+            // the CG loop...
+            while (iter < p.pcg_max_iter && delta_new > p.pcg_tol * delta_0) {
+                // q = A d
+                for (jj = 0; jj < n; ++jj) {
+                    q[jj] = 0.0;
+                    for (kk = 0; kk < n; ++kk) {
+                        q[jj] += sddn_element_func(jj, kk) * d[kk];
+                    }
+                }
+
+                // alpha = delta_new / (d^T q)
+                double denom = 0.0;
+                for (jj = 0; jj < n; ++jj) {
+                    denom += d[jj] * q[jj];
+                }
+                double alpha = delta_new / denom;
+
+                // x = x + alpha d
+                for (jj = 0; jj < n; ++jj) {
+                    a_jk[jj] += alpha * d[jj];
+                }
+
+                // Update residual.
+                // Approximate update.
+                // r = r - alpha q
+                for (jj = 0; jj < n; ++jj) {
+                    r[jj] -= alpha * q[jj];
+                }
+
+                // reapply preconditioner.
+                for (jj = 0; jj < n; ++jj) {
+                    s[jj] = r[jj] / sddn_element_func(jj, jj);
+                }
+
+                // save current delta.
+                double delta_old = delta_new;
+
+                // Update delta.
+                // delta_new = r^T s
+                delta_new = 0.0;
+                for (jj = 0; jj < n; ++jj) {
+                    delta_new += r[jj] * s[jj];
+                }
+
+                // Update d.
+                double beta = delta_new / delta_old;
+                for (jj = 0; jj < n; ++jj) {
+                    d[jj] = s[jj] + beta * d[jj];
+                }
+
+                // Finally, update the count.
+                ++iter;
+            }
+
+            // Dot with data vector.
+            double b = 0.0;
+            for (k = 0; k < n; ++k) {
+                b += a_jk[k] * data_vec[k];
+            }
+
+            // Save this pixel result.
+            b_j[j] = b;
+        } // end omp for
+
+        // don't forget to free the thread work arrays.
+        delete [] d;
+        delete [] r;
+        delete [] q;
+        delete [] s;
+        delete [] a_jk;
+
+    } // end omp
+
+    printf("Main loop time: %g ms.\n", loop_timer->elapsed());
+
+    delete [] noise_vec;
 
     //
-    // Create matrix lookups.
+    // Second loop to compute S b
     //
 
-    double (*f)(int, int, void *);
-
-    // S^MD matrix
-    // params.
-    SMDParams *p1 = new SMDParams;
-    p1->nx = nx;
-    p1->ny = ny;
-    p1->nz = nz;
-    p1->dx = dx;
-    p1->dy = dy;
-    p1->dz = dz;
-    p1->ns = num_skewers;
-    p1->np = num_pixels;
-    p1->dz_pix = dz_pix;
-    p1->skewer_x = skewer_x;
-    p1->skewer_y = skewer_y;
-    p1->l_perp = l_perp;
-    p1->l_para = l_para;
-    p1->sigma = sigma_F;
-    p1->gt = gt;
-
-    // Grab function pointer.
-    f = &smd_element_func;
-
-    LUM *smd = new LUM(num_cells, num_pixel_elements, f, p1);
-
-    // S^DD + N matrix
-    // params.
-    SDDNParams *p2 = new SDDNParams;
-    p2->ns = num_skewers;
-    p2->np = num_pixels;
-    p2->dz_pix = dz_pix;
-    p2->skewer_x = skewer_x;
-    p2->skewer_y = skewer_y;
-    p2->l_perp = l_perp;
-    p2->l_para = l_para;
-    p2->sigma = sigma_F;
-    p2->n = noise_vec;
-    p2->gt = gt;
-
-    // Grab function pointer.
-    f = &sddn_element_func;
-
-    LUM *sddn = new LUM(num_pixel_elements, num_pixel_elements, f, p2);
-
-    // Allocate work arrays.
-    double *a_j = new double[num_pixel_elements];
-    double *d = new double[num_pixel_elements];
-    double *r = new double[num_pixel_elements];
-    double *s = new double[num_pixel_elements];
-    double *q = new double[num_pixel_elements];
     // Allocate map.
     double *map_vec = new double[num_cells];
 
@@ -268,39 +380,18 @@ main(int argc, char **argv)
         map_vec[i] = 0.0;
     }
 
-    printf("Entering main loop with n_i = %i, n_j = %i.\n",
-        (int)num_cells, (int)num_pixel_elements);
-
-    // Set initial a_j guess.
-    for (int i = 0; i < num_pixel_elements; ++i) {
-        if (i == 0) {
-            a_j[i] = 1.0 / (*sddn)(i, i);
-        }
-        else {
-            a_j[i] = 0.0;
+    // Each thread gets a block of cells.
+    #pragma omp parallel for
+    for (int i = 0; i < num_cells; ++i) {
+        for (int j = 0; j < num_pixel_elements; ++j) {
+            map_vec[i] += smd_element_func(i, j) * b_j[j];
         }
     }
 
-    // The main loop, first over cells, then pixels.
-    for (int j = 0; j < num_pixel_elements; ++j) {
-
-        // DEBUG
-        printf("pixel %i -- %.2f %%\n", (int)j, 100.0*j/num_pixel_elements);
-
-        // PCG for the j-th row of (S^DD + N)^-1.
-        pcg_invert(j, sddn, a_j, pcg_max_iter, pcg_tol, d, r, s, q);
-
-        // Dot with data vector.
-        double b_j = 0.0;
-        for (int k = 0; k < num_pixel_elements; ++k) {
-            b_j += a_j[k] * data_vec[k];
-        }
-
-        // Add contribution to map.
-        for (int i = 0; i < num_cells; ++i) {
-            map_vec[i] += (*smd)(i, j) * b_j;
-        }
-    }
+    delete [] skewer_x;
+    delete [] skewer_y;
+    delete [] data_vec;
+    delete gt;
 
     // Write map field.
     printf("Writing map file %s.\n", output_path.c_str());
@@ -308,24 +399,9 @@ main(int argc, char **argv)
     fwrite(map_vec, sizeof(double), num_cells, outfile);
     fclose(outfile);
 
-    // So valgrind doesn't whine...
-    delete [] skewer_x;
-    delete [] skewer_y;
-    delete [] data_vec;
-    delete [] noise_vec;
-
-    delete sddn;
-    delete smd;
-    delete p1;
-    delete p2;
-    delete gt;
-
     delete [] map_vec;
-    delete [] a_j;
-    delete [] d;
-    delete [] q;
-    delete [] r;
-    delete [] s;
+
+    printf("Total time: %g ms.\n", total_timer->elapsed());
 
     return 0;
 }
