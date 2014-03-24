95f7e8ac373456739ed830326a05581c00e34342
Casey W. Stark <caseywstark@gmail.com>
2014-03-24 15:30:03 -0700

    Updating gaussian blob test case.

--- examples/gaussian/generate_input.py
+++ examples/gaussian/generate_input.py
@@ -1,40 +1,116 @@
 import numpy as np
 
+# domain size
 l = 1.0
-n = 4
+# gaussian center and size
+c = 0.5
+sigma = 0.3
 
+# full map
+map_n = 7
+dx = l / map_n
+ii = np.mgrid[0:map_n, 0:map_n, 0:map_n]
+xx = dx * (ii + 0.5)
+rr = xx - c
+r2 = rr[0]*rr[0] + rr[1]*rr[1] + rr[2]*rr[2]
+g = np.exp(-r2/sigma**2)
+g_mean = g.mean()
+dg = g / g_mean - 1
+
+dg.tofile("ideal_signal.bin")
+
+# skewer setup
+
+n = 4
 num_skewers = n*n
 num_pixels = n
 pix_n = num_skewers * num_pixels
 
 dx = l / n
+dy = l / n
+dz = l / num_pixels
 
 # skewer pos.
-sx = np.zeros([num_skewers])
-sy = np.zeros([num_skewers])
+sx = np.zeros(num_skewers)
+sy = np.zeros(num_skewers)
+d = np.zeros((num_skewers, num_pixels))
 
-i = 0
+isk = 0
 for ix in xrange(n):
     for iy in xrange(n):
-        sx[i] = dx * (ix + 0.5)
-        sy[i] = dx * (iy + 0.5)
-        i += 1
+        x = dx * (ix + 0.5)
+        y = dx * (iy + 0.5)
+        for iz in xrange(num_pixels):
+            z = dz * (iz + 0.5)
+            rx = x - c
+            ry = y - c
+            rz = z - c
+            r2 = rx*rx + ry*ry + rz*rz
+            d[isk, iz] = np.exp(-r2 / sigma**2)
+        sx[isk] = x
+        sy[isk] = y
+        isk += 1
 
-# gaussian center
-c = 0.5
-
-# set data and weights.
-ii = np.mgrid[0:n, 0:n, 0:n]
-xx = dx * (ii + 0.5)
-rr = xx - c
-r2 = rr[0]*rr[0] + rr[1]*rr[1] + rr[2]*rr[2]
-g = np.exp(-r2/0.2**2)
-
-w = np.ones(g.shape)
+dd = d / g_mean - 1
+w = np.ones(d.shape)
 
 # write out
 sx.tofile("skewer_x.bin")
 sy.tofile("skewer_y.bin")
-g.tofile("pixel_data.bin")
+dd.tofile("pixel_data.bin")
 w.tofile("pixel_weights.bin")
 
+var_s = dd.std()**2
+print "var_s = %g" % var_s
+
+x_perp_2_list = []
+x_para_2_list = []
+s_list = []
+ss_list = []
+
+x2_list = []
+cs_list = []
+
+i = 0
+for ixi in xrange(n):
+    xi = dx * (ixi + 0.5)
+    for iyi in xrange(n):
+        yi = dy * (iyi + 0.5)
+        for izi in xrange(num_pixels):
+            zi = dz * (izi + 0.5)
+            i = ixi * n + iyi
+            di = dd[i, izi]
+
+            for ixj in xrange(n):
+                xj = dx * (ixj + 0.5)
+                dx_ij = xj - xi
+
+                for iyj in xrange(n):
+                    yj = dy * (iyj + 0.5)
+                    dy_ij = yj - yi
+                    x_perp_2 = (dx_ij*dx_ij + dy_ij*dy_ij)
+
+                    for izj in xrange(num_pixels):
+                        zj = dz * (izj + 0.5)
+                        dz_ij = zj - zi
+                        x_para_2 = dz_ij*dz_ij
+
+                        j = ixj * n + iyj
+                        dj = dd[j, izj]
+                        sij = di * dj
+
+                        ssij = var_s * np.exp(-x_para_2/sigma) * np.exp(-x_perp_2/sigma)
+
+                        x_perp_2_list.append(x_perp_2)
+                        x_para_2_list.append(x_para_2)
+                        s_list.append(sij)
+                        ss_list.append(ssij)
+
+                        x2 = dx_ij*dx_ij + dy_ij*dy_ij + dz_ij*dz_ij
+                        x2_list.append(x2)
+
+x_perp_2 = np.array(x_perp_2_list)
+x_para_2 = np.array(x_para_2_list)
+x2 = np.array(x2_list)
+s = np.array(s_list)
+ss = np.array(ss_list)
--- examples/gaussian/plot_map_slices.py
+++ examples/gaussian/plot_map_slices.py
@@ -1,32 +1,118 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+lx = 1.0
+ly = 1.0
+lz = 1.0
 nx = 7
 ny = 7
 nz = 7
-nsk = 16
 
-m = np.fromfile("map.bin").reshape((nx, ny, nz))
-sc = np.fromfile("skewer_coords.bin").reshape((2, nsk))
+sx = np.fromfile("skewer_x.bin")
+sy = np.fromfile("skewer_y.bin")
+ideal_signal = np.fromfile("ideal_signal.bin").reshape(nx, ny, nz)
+m = np.fromfile("map.bin").reshape(nx, ny, nz)
+mcov_nd = np.fromfile("map_covar_nd.bin").reshape((nx, ny, nz))
 
-e = (0, 1, 0, 1)
-vmin = m.min()
-vmax = m.max()
+s_min = ideal_signal.min()
+s_max = ideal_signal.max()
+mc_min = mcov_nd.min()
+mc_max = mcov_nd.max()
 
-for i in xrange(nz):
-    print i
-    fig = plt.figure(figsize=(6, 5))
-    ax = fig.add_subplot(111)
+# Figure params
+fig_w = 8.0
+fig_h = 3.0
+aspect = fig_w / fig_h
 
-    im = ax.imshow(m[:, :, i], vmin=vmin, vmax=vmax, extent=e, aspect="equal")
-    p = ax.plot(sc[0], sc[1], ls="", marker=".", ms=5, c="black", alpha=0.5)
-    cb = plt.colorbar(im)
-    ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$y$")
+l = 0.08
+b = 0.14
+im_w = 0.22
+im_h = aspect * im_w
+im_wpad = 0.0
+im_hpad = 0.0
+cb_w = im_w
+cb_h = 0.02
+cb_wpad = (im_w - cb_w) / 2
+lp = -37
 
-    im.set_rasterized(True)
-    cb.solids.set_rasterized(True)
+for ix in xrange(0, nx):
+    print "plotting slice ix = %i" % ix
+    fig = plt.figure(figsize=(fig_w, fig_h))
 
-    plt.tight_layout()
-    plt.savefig("zslice_%i.pdf" % i, dpi=100)
+    ax1 = fig.add_axes((l, b, im_w, im_h))
+    ax2 = fig.add_axes((l + im_w + im_wpad, b, im_w, im_h))
+    ax3 = fig.add_axes((l + 2*(im_w + im_wpad), b, im_w, im_h))
+    ax4 = fig.add_axes((l + 3*(im_w + im_wpad), b, im_w, im_h))
+
+    cax1 = fig.add_axes((l + cb_wpad, b + im_h + im_hpad, cb_w, cb_h))
+    cax2 = fig.add_axes((l + im_w + im_wpad + cb_wpad, b + im_h + im_hpad, cb_w, cb_h))
+    cax3 = fig.add_axes((l + 2*(im_w + im_wpad) + cb_wpad, b + im_h + im_hpad, cb_w, cb_h))
+    cax4 = fig.add_axes((l + 3*(im_w + im_wpad) + cb_wpad, b + im_h + im_hpad, cb_w, cb_h))
+
+    # skewer position plot
+    xx = lx / nx * (ix + 0.5)
+    ax1.axvline(xx, ls="-", lw=1, c="k", alpha=0.5)
+    s = ax1.scatter(sx, sy, c=np.ones(sx.size), marker="s", s=5, linewidths=0.0, alpha=0.7)
+    ax1.set_xlabel(r"$x$")
+    ax1.set_ylabel(r"$y$")
+
+    cb1 = plt.colorbar(s, cax=cax1, orientation="horizontal")
+    cb1.solids.set_rasterized(True)
+    cax1.tick_params(bottom=False, labelbottom=False, top=True, labeltop=True)
+    cb1.set_label(r"$\rm skewer \, pos/weights$", labelpad=lp)
+
+    # simulation delta f
+    im2 = ax2.imshow(ideal_signal[ix], vmin=s_min, vmax=s_max,
+        extent=(0, lz, 0, ly), aspect="equal")
+    im2.set_rasterized(True)
+    ax2.set_xlabel(r"$z$")
+    ax2.tick_params(labelleft=False)
+
+    cb2 = plt.colorbar(im2, cax=cax2, orientation="horizontal")
+    cb2.solids.set_rasterized(True)
+    cax2.tick_params(bottom=False, labelbottom=False, top=True, labeltop=True)
+    cb2.set_label(r"$\mathrm{ideal} \, s$", labelpad=lp)
+
+    # reconstruction
+    im3 = ax3.imshow(m[ix], vmin=s_min, vmax=s_max, extent=(0, lz, 0, ly),
+        aspect="equal")
+    im3.set_rasterized(True)
+    ax3.set_xlabel(r"$z$")
+    ax3.tick_params(labelleft=False)
+
+    cb3 = plt.colorbar(im3, cax=cax3, orientation="horizontal")
+    cb3.solids.set_rasterized(True)
+    cax3.tick_params(bottom=False, labelbottom=False, top=True, labeltop=True)
+    cb3.set_label(r"$\mathrm{reconstruction} \, s$", labelpad=lp)
+
+    # covar plot
+    im4 = ax4.imshow(mcov_nd[ix], vmin=mc_min, vmax=mc_max,
+        extent=(0, lz, 0, ly), aspect="equal")
+    im4.set_rasterized(True)
+    ax4.set_xlabel(r"$z$")
+    ax4.tick_params(labelleft=False)
+
+    cb4 = plt.colorbar(im4, cax=cax4, orientation="horizontal")
+    cb4.solids.set_rasterized(True)
+    cb4.set_label(r"$\lim_{N \gg S} M_{ii}$", labelpad=lp)
+    cax4.tick_params(bottom=False, labelbottom=False, top=True, labeltop=True)
+
+    ax1.set_xlim(0, lx)
+    ax1.set_ylim(0, ly)
+    ax2.set_xlim(0, lz)
+    ax2.set_ylim(0, ly)
+    ax3.set_xlim(0, lz)
+    ax3.set_ylim(0, ly)
+    ax4.set_xlim(0, lz)
+    ax4.set_ylim(0, ly)
+
+    #ax2.set_xticks([20, 40, 60, 80, 100])
+    #ax3.set_xticks([20, 40, 60, 80, 100])
+    #ax4.set_xticks([20, 40, 60, 80, 100])
+    #cb1.set_ticks([0, 250, 500])
+    #cb2.set_ticks([-0.8, -0.4, 0])
+    #cb3.set_ticks([-0.8, -0.4, 0])
+    #cb4.set_ticks([0, 1, 2])
+
+    plt.savefig("map_x%02d.pdf" % ix, dpi=100)
     plt.close()
